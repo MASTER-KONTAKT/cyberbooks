32616a0c63b1b4095c01424b4b9dc56e1b046800
MASTER-KONTAKT <129795136+MASTER-KONTAKT@users.noreply.github.com>
2026-05-19 20:26:27 +0200

    Add files via upload

---
+++ slownik.js
@@ -0,0 +1,9 @@
+{
+  "tom": "Tom (imię głównego bohatera)",
+  "toms": "Toma (forma dzierżawcza, np. Tom's house - dom Toma)",
+  "green": "Zielona (część nazwy ulicy 'Green Street' lub szkoły)",
+  "street": "Ulica",
+  "chapter": "Rozdział",
+  "city": "Miasto",
+  "beautiful": "Piękna / Piękny"
+}
